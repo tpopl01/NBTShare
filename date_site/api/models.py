36f63578c6707d571bc936db9dfc532e903a2319
Tim Pople <timothypople@yahoo.co.uk>
2020-11-24 19:44:55 +0000

    Profile table and NBT table created

--- date_site/api/models.py
+++ date_site/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    message = models.CharField(max_length=500, blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class NBTFile(models.Model):
+    name = models.CharField(max_length=50, unique=False)
+    description = models.CharField(max_length=600, unique=False)
+    download_count = models.IntegerField(null=False, default=0)
+    created_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
